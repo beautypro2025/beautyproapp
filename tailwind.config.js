e63adb760ce09640964838330f1dac3163a4ee58
joab102011 <beautyproatendimento@gmail.com>
2025-04-10 17:35:44 -0300

    Ajustes para build correto

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        playfair: ['var(--font-playfair)'],
+        poppins: ['var(--font-poppins)'],
+      },
+      colors: {
+        primary: "#b5715f",
+        "primary-dark": "#8b574a",
+        background: "#FDF8F6"
+      }
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+} 
